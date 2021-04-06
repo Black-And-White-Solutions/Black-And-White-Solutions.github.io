e63e2a1a86b3ae5a9bbdbe64d37d423263b313ee
GGabrielDev <luisgabrielramosrobles@gmail.com>
2021-04-06 11:56:13 -0400

    Footer first iteration

---
+++ src/components/Footer.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import iconFacebook from "../img/Facebook logo.png";
+import iconTwitter from "../img/Twitter logo.png";
+import iconLinkedIn from "../img/LinkedIn logo.png";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footerBar">
+      <p className="footerText">
+        2020 Black & White Solutions, todos los derechos reservados.
+      </p>
+      <div className="footerSocial">
+        <a href="https://www.facebook.com/Black-And-White-Solutions-112478920935033/">
+          <img
+            className="icon leftIcon"
+            src={iconFacebook}
+            alt="iconFacebook"
+          />
+        </a>
+        <a href="https://twitter.com/BnW_Solutions">
+          <img
+            className="icon centerIcon"
+            src={iconTwitter}
+            alt="iconTwitter"
+          />
+        </a>
+        <a href="https://www.linkedin.com">
+          <img
+            className="icon rightIcon"
+            src={iconLinkedIn}
+            alt="iconLinkedIn"
+          />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;

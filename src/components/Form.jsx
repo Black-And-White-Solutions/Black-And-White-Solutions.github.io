e7daaf00a5d1507f954609e8f1c4aeadc81a7c48
GGabrielDev <luisgabrielramosrobles@gmail.com>
2021-04-16 21:10:42 -0400

    Newer iteration of Contact form - Added Booststrap

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,40 +9,44 @@ const Form = ({ formId }) => {
 
   return (
     <div className="container-fluid bg">
-      <div className="mainCard">
-        <div className="formMain">
-          <form id={formId} onSubmit={handleSubmit} className="form">
-            <fieldset>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                placeHolder="Nombre"
-                className="font textBox"
-              />
-              <input
-                type="text"
-                name="objective"
-                id="objective"
-                placeholder="Objetivo del proyecto"
-                className="font textBox"
-              />
-              <input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="E-mail"
-                className="font textBox"
-              />
-              <input
-                type="tel"
-                name="telephone"
-                id="telephone"
-                placeholder="Teléfono"
-                className="font textBox"
-              />
-            </fieldset>
-          </form>
+      <div className="container row mainCard">
+        <h1 className="row h-9 formTitle">Contacta con nosotros</h1>
+        <div className="row">
+          <div className="col-sm formText">GG</div>
+          <div className="col-sm formMain">
+            <form id={formId} onSubmit={handleSubmit} className="form">
+              <fieldset className="row">
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeHolder="Nombre"
+                  className="row-sm font textBox"
+                />
+                <input
+                  type="text"
+                  name="objective"
+                  id="objective"
+                  placeholder="Objetivo del proyecto"
+                  className="row-sm font textBox"
+                />
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="E-mail"
+                  className="row-sm font textBox"
+                />
+                <input
+                  type="tel"
+                  name="telephone"
+                  id="telephone"
+                  placeholder="Teléfono"
+                  className="row-sm font textBox"
+                />
+              </fieldset>
+            </form>
+          </div>
         </div>
       </div>
       <button type="submit" className="font bold submitButton">

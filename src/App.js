7b1df8292339c6202ca19cd83ae2c6adc897e8c5
danieljml <danielgarrido425@gmail.com>
2021-04-28 13:25:11 -0400

    commit de prueba

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar.jsx';
 import Presentation from './components/Presentation.jsx';
 import Services from './components/Services.jsx';
-import Proceso from './components/Proceso';
+import Process from './components/Process';
 import Team from './components/Team';
-
 
 function App() {
   return (
@@ -12,9 +12,9 @@ function App() {
       <Navbar />
       <Presentation />
       <Services />
-      <Proceso />
-      <Team /> 
-   </div>
+      <Process />
+      <Team />
+    </div>
   );
 }
 
--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Styles/Proceso.css' 
+import './Styles/Process.css' 
 
 const Proceso = () => {
 

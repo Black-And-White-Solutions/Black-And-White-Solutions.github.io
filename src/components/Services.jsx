b59d0f8b0efd4e068fb8a08277d1fc28947167f3
danieljml <danielgarrido425@gmail.com>
2021-05-03 19:16:11 -0400

    Services first refactor

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,83 +1,32 @@
 import React from 'react';
-import './Styles/Services.css';
+import ServicesCard from './ServicesCard';
+import './styles/Services.css';
+import listOfServices from '../data/listOfServices.json';
 
 const Services = () => {
+  const servicesCards = [];
+  listOfServices.forEach((item, index) => {
+    const index_is_even = index % 3 == 0;
+    servicesCards.push(
+      <ServicesCard
+        key={index}
+        cardColor={index_is_even ? 'white' : 'black'}
+        content={item.content}
+        delay={item.delay}
+        iconCSS={item.iconCSS}
+        title={item.title}
+      />,
+    );
+  });
   return (
-    <>
-      <div className="container-fluid service" data-aos="fade-up">
-        <header className="section-header pt-5">
-          <h2>Servicios</h2>
-        </header>
-        <div className="container mb-5">
-          <div className="row gy-3">
-            <div
-              className="col-sm-7 col-md-6"
-              data-aos="fade-up"
-              data-aos-delay="200">
-              <div className="serviceCardWhite p-5">
-                <h3>
-                  <i className="fas fa-users me-3 iconServices"></i>Equipos
-                  dedicados
-                </h3>
-                <p>
-                  Construye tu producto con un equipo dedicado de
-                  desarrolladores que estarán disponibles para ti a lo largo de
-                  todo el ciclo de vida del proyecto.
-                </p>
-              </div>
-            </div>
-            <div
-              className="col-sm-7 col-md-6"
-              data-aos="fade-up"
-              data-aos-delay="300">
-              <div className="serviceCardBlack p-5">
-                <h3>
-                  <i className="fas fa-tools me-3 iconServices"></i>Desarrollo
-                  de MVPs
-                </h3>
-                <p>
-                  ¿Tienes alguna idea y te gustaría evaluar su víabilidad?
-                  Podemos desarrollar un producto minimo viable que podras usar
-                  para plasmar tus ideas en la realidad.
-                </p>
-              </div>
-            </div>
-            <div
-              className="col-sm-7 col-md-6"
-              data-aos="fade-up"
-              data-aos-delay="400">
-              <div className="serviceCardBlack p-5">
-                <h3>
-                  <i className="fas fa-globe me-3 iconServices"></i>Desarrollo
-                  de Apps
-                </h3>
-                <p>
-                  ¿Quieres darle una imágen a tu proyecto? Tenemos todo un
-                  equipo preparado para entender tu negocio y crear un sitio web
-                  atractivo y amigable para tus clientes.
-                </p>
-              </div>
-            </div>
-            <div
-              className="col-sm-7 col-md-6"
-              data-aos="fade-up"
-              data-aos-delay="500">
-              <div className="serviceCardWhite p-5">
-                <h3>
-                  <i className="fas fa-cogs me-3 iconServices"></i>Desarrollo de
-                  servicios
-                </h3>
-                <p>
-                  Nuestros desarrolladores cuentan con conocimeintos de
-                  desarrollo Backend, con lo que podemos crear sistemas
-                  vanguardistas que alimenten y ayuden a optimizar tu negocio.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div className="container-fluid services" data-aos="fade-up">
+      <header className="section-header pt-5">
+        <h2>Servicios</h2>
+      </header>
+      <div className="container mb-5">
+        <div className="row gy-3">{servicesCards}</div>
       </div>
-    </>
+    </div>
   );
 };
 
